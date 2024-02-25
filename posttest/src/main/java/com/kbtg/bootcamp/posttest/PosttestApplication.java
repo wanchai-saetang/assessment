a8918ba43231469a6be0224e90913d78310cbab1
Wanchai Saetang <twansaetang@gmail.com>
2024-02-25 13:21:19 +0700

    edit ci working directory and test precommit

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -2,6 +2,7 @@ package com.kbtg.bootcamp.posttest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PosttestApplication {
